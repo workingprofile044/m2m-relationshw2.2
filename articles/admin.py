fa69e43ec1fe4c35e9e8e4bf748e83ab3336c9a9
workingprofile044 <sengokubatsu@gmail.com>
2023-08-31 10:30:23 +0800

    fixed admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -1,16 +1,36 @@
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 from django.contrib import admin
 from .models import Article, Tag, Scope
 
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                main_count += 1
+
+        if main_count != 1:
+            raise ValidationError('Должен быть указан основной раздел')
+
+        return super().clean()
+
+
 class ScopeInline(admin.TabularInline):
     model = Scope
+    formset = ScopeInlineFormset
+
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [ScopeInline]
 
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     pass
+
 
 @admin.register(Scope)
 class ScopeAdmin(admin.ModelAdmin):
